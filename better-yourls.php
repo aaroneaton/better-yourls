fd65eec95d2a76a099b88ec180c16013a7c6f7f3
Chris Wiegman <chris@chriswiegman.com>
2014-10-23 17:46:34 -0500

    Increment version number

--- better-yourls.php
+++ better-yourls.php
@@ -3,7 +3,7 @@
 	Plugin Name: Better YOURLS
 	Plugin URI: http://chriswiegman.com
 	Description: Replace WordPress shortlinks with custom YOURLS domain.
-	Version: 1.0.1
+	Version: 1.0.2
 	Text Domain: better-yourls
 	Domain Path: /languages
 	Author: Chris Wiegman
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: https://chriswiegman.com/donate
 Tags: yourls, shortlink, custom shortlink
 Requires at least: 3.5
 Tested up to: 4.0
-Stable tag: 1.0.1
+Stable tag: 1.0.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 

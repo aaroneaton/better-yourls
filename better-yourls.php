979a8f8f53f20ae48c4810916265bfda22f5633c
Chris Wiegman <chris@chriswiegman.com>
2015-07-22 20:29:45 -0400

    More cleanup

--- better-yourls.php
+++ better-yourls.php
@@ -15,16 +15,28 @@ Copyright 2015 Chris Wiegman  (email: info@chriswiegman.com)
 define( 'BYOURLS_VERSION', '1.0.5' );
 define( 'BYOURLS_URL', plugin_dir_url( __FILE__ ) );
 
+//Load front end items
 if ( ! class_exists( 'Better_YOURLS_Actions' ) ) {
-
 	require( dirname( __FILE__ ) . '/classes/class-better-yourls-actions.php' );
-	new Better_Yourls_Actions();
-
 }
 
-if ( is_admin() && ! class_exists( 'Better_YOURLS_Admin' ) ) {
+new Better_Yourls_Actions();
 
-	require( dirname( __FILE__ ) . '/classes/class-better-yourls-admin.php' );
+//Load Dashboard items
+if ( is_admin() ) {
+
+	if ( ! class_exists( 'Better_YOURLS_Admin' ) ) {
+		require( dirname( __FILE__ ) . '/classes/class-better-yourls-admin.php' );
+	}
+
 	new Better_Yourls_Admin();
 
 }
+
+//Handle activation and deactivation
+if ( ! class_exists( 'Better_YOURLS_Setup' ) ) {
+	require( dirname( __FILE__ ) . '/classes/class-better-yourls-setup.php' );
+}
+
+register_activation_hook( __FILE__, array( 'Better_YOURLS_Setup', 'on_activate' ) );
+register_deactivation_hook( __FILE__, array( 'Better_YOURLS_Setup', 'on_deactivate' ) );

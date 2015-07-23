35dc9db5709df0ac2ccb184a67b66c49abe62af0
Chris Wiegman <chris@chriswiegman.com>
2015-07-22 21:29:42 -0400

    Always load the plugin domain
    Always make certain the plugin domain loads regardless of selected options.

--- classes/class-better-yourls-actions.php
+++ classes/class-better-yourls-actions.php
@@ -44,9 +44,10 @@ class Better_YOURLS_Actions {
 			add_action( 'admin_bar_menu', array( $this, 'admin_bar_menu' ), 100 );
 			add_action( 'transition_post_status', array( $this, 'transition_post_status' ), 10, 3 );
 			add_action( 'wp_enqueue_scripts', array( $this, 'wp_enqueue_scripts' ) );
-			add_action( 'plugins_loaded', array( $this, 'action_plugins_loaded' ) );
 
 		}
+
+		add_action( 'plugins_loaded', array( $this, 'action_plugins_loaded' ) );
 
 	}
 

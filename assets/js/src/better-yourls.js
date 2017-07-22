726086998694a889b32e5e2cb78738d181ca8d9d
Chris Wiegman <chris@wiegman.us>
2017-07-22 14:07:28 -0400

    Add JS sources

---
+++ assets/js/src/better-yourls.js
@@ -0,0 +1,9 @@
+jQuery( document ).ready( function () {
+
+	jQuery( '#wp-admin-bar-better_yourls-link' ).click( function () {
+
+		alert( better_yourls.text + '\n\n' + better_yourls.yourls_url );
+
+	} );
+
+} );

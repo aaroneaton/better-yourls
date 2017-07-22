06e2150c35beb0385260cf2c532174b23977c623
Chris Wiegman <chris@wiegman.us>
2017-07-22 14:35:20 -0400

    Explicitly exclude production map file

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,6 +37,7 @@ module.exports = function (grunt) {
 					options: {
 						beautify:         false,
 						preserveComments: false,
+						sourceMap:        false,
 						mangle:           {
 							reserved: ['jQuery']
 						}

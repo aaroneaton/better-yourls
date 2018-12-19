7a7c756d1dce2254fdf9e01566b0fb9ceeb76269
J. Aaron Eaton <aaron@channeleaton.com>
2018-12-19 07:03:55 -0600

    Showing panel in pre-publish area

--- assets/src/gutenberg-metabox/index.js
+++ assets/src/gutenberg-metabox/index.js
@@ -1,35 +1,34 @@
 import React, { Fragment } from 'react';
+import { TextControl } from '@wordpress/components';
+import { withState } from '@wordpress/compose';
 
 // Get the components to add custom menu items and sidebars
-const { PluginSidebar, PluginSidebarMoreMenuItem } = wp.editPost;
+const { PluginPrePublishPanel } = wp.editPost;
 
 // Export the plugin name
-export const name = 'starter-kit-plugin';
+export const name = 'better-yourls';
 
-// Create a basic plugin placeholder with
-// more menu item and a custom sidebar
+const YourlsTextControl = withState({
+		value: '',
+})( ( { value, setState } ) => (
+	<TextControl
+		label={'YOURLs Keyword'}
+		value={value}
+		onChange={ ( value ) => setState( { value } ) }
+	/>
+));
+
 export const settings = {
-
-	// The plugin icon
-	icon: 'smiley',
-
-	// This function returns the component to render
 	render() {
 		return (
-			<Fragment>
-				<PluginSidebarMoreMenuItem
-					target="example-plugin"
-				>
-					Example Plugin
-				</PluginSidebarMoreMenuItem>
-				<PluginSidebar
-					name="example-plugin"
-					title="Example Plugin"
-				>
-					Content of the example plugin sidebar
-				</PluginSidebar>
-			</Fragment>
+			<PluginPrePublishPanel
+				className="better-yourls-pre-publish-panel"
+				title={'Better YOURLs'}
+				initialOpen={true}
+			>
+				<YourlsTextControl/>
+			</PluginPrePublishPanel>
 		)
-	},
+	}
+}
 
-};

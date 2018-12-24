9bf2b226e7c1180daaeff7f9a8011284bca6706e
J. Aaron Eaton <aaron@channeleaton.com>
2018-12-24 09:33:49 -0600

    Added email subscription box

--- includes/class-better-yourls-admin.php
+++ includes/class-better-yourls-admin.php
@@ -85,6 +85,14 @@ class Better_YOURLS_Admin {
 		);
 
 		add_meta_box(
+			'better_yourls_subscribe',
+			esc_html__( 'Subscribe', 'better-yourls' ),
+			[ $this, 'metaboxSubscribe' ],
+			'settings_page_better_yourls',
+			'side'
+		);
+
+		add_meta_box(
 			'better_yourls_support',
 			esc_html__( 'Support This Plugin', 'better-yourls' ),
 			array( $this, 'metabox_support' ),
@@ -243,8 +251,6 @@ class Better_YOURLS_Admin {
 	 */
 	public function metabox_settings() {
 
-		echo '<p><strong>This plugin is up for adoption. Please <a href="http://www.chriswiegman.com/contact/">contact me</a> if you would like to continue its development.</strong></p>';
-
 		echo '<p>', esc_html__( 'Use the settings below to configure Better YOURLs for your site.', 'better-yourls' ), '</p>';
 
 		?>
@@ -257,6 +263,60 @@ class Better_YOURLS_Admin {
 
 	<?php
 
+	}
+
+	/**
+	 * Builds a email subscription box
+	 *
+	 * @since 2.3.0
+	 *
+	 * @return void
+    */
+	public function metaboxSubscribe() {
+	    ?>
+	   <!-- Begin Mailchimp Signup Form -->
+		<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+		<style type="text/css">
+			#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+			/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+			   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+		</style>
+		<div id="mc_embed_signup">
+		<form></form>
+		<form action="https://blog.us8.list-manage.com/subscribe/post?u=d6c5983af28b0d029985583e9&amp;id=af9099f5d0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+			<div id="mc_embed_signup_scroll">
+			<h2>Subscribe to our mailing list</h2>
+		<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+		<div class="mc-field-group">
+			<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+		</label>
+			<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+		</div>
+		<div class="mc-field-group">
+			<label for="mce-FNAME">First Name </label>
+			<input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+		</div>
+		<div class="mc-field-group">
+			<label for="mce-LNAME">Last Name </label>
+			<input type="text" value="" name="LNAME" class="" id="mce-LNAME">
+		</div>
+		<div class="mc-field-group input-group" style="display: none;">
+			<strong>Signup Form </strong>
+			<ul><li><input type="checkbox" value="1" name="group[84885][1]" id="mce-group[84885]-84885-0" checked><label for="mce-group[84885]-84885-0" checked>Better YOURLs WP Admin</label></li>
+		</ul>
+		</div>
+			<div id="mce-responses" class="clear">
+				<div class="response" id="mce-error-response" style="display:none"></div>
+				<div class="response" id="mce-success-response" style="display:none"></div>
+			</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+			<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_d6c5983af28b0d029985583e9_af9099f5d0" tabindex="-1" value=""></div>
+			<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+			</div>
+		</form>
+		</div>
+		<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+		<!--End mc_embed_signup-->
+	    <?php
 	}
 
 	/**
